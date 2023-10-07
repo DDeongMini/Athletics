1f2b9fa68b781aace1b03ed481c484ab17a33f33
DDeongMini <has3257@naver.com>
2023-10-08 03:32:35 +0900

    first commit

---
+++ build/dynamixel_current_2port/CMakeFiles/Move_decision_node.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/sm/Athletics/devel/lib/dynamixel_current_2port/Move_decision_node"
+  "/home/sm/Athletics/devel/lib/dynamixel_current_2port/Move_decision_node.pdb"
+  "CMakeFiles/Move_decision_node.dir/src/Move_decision2.cpp.o"
+  "CMakeFiles/Move_decision_node.dir/src/Move_decision_node.cpp.o"
+  "CMakeFiles/Move_decision_node.dir/src/img_proc2.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Move_decision_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/dynamixel_current_2port/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sm/Athletics/src/dynamixel_current_2port
+# Build directory: /home/sm/Athletics/build/dynamixel_current_2port
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
